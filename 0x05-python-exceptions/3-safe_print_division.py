44f53d62e0d416265375aa6062c4778a71913f72
Tr3v1n4t0r <813@holbertonschool.com>
2020-01-08 15:53:39 -0800

    3 done

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    sum = None
+    try:
+        sum = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {}".format(sum))
+        return sum
