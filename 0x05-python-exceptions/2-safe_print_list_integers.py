20f7720f14315ec129ecd719a0770dbbcb5c873f
Tr3v1n4t0r <813@holbertonschool.com>
2020-01-08 15:37:30 -0800

    2 done better than wanted

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        a, b = 0, 0
+        while a < x:
+            try:
+                print("{:d}".format(my_list[a]), end='')
+                a += 1
+                b += 1
+            except:
+                a += 1
+        print()
+    except:
+        print()
+    return b
