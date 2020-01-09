d71ab6a5b3ed7c8a80a73a3453c2173f3680b660
Tr3v1n4t0r <813@holbertonschool.com>
2020-01-09 15:55:32 -0800

    changed words to match

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,9 +10,9 @@ class Square:
     @size.setter
     def size(self, value):
         if type(value) != int:
-            raise TypeError('value must be an integer')
+            raise TypeError('size must be an integer')
         if value < 0:
-            raise ValueError('value must be >= 0')
+            raise ValueError('size must be >= 0')
         self.__size = value
 
     def area(self):
