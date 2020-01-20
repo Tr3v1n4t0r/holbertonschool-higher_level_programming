022d5a65ed30ef31aae22939c07dab8c39b70577
Tr3v1n4t0r <813@holbertonschool.com>
2020-01-19 18:33:00 -0800

    replaced words with value to make it work

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -17,9 +17,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Sets the width"""
-        if type(width) is not int:
+        if type(value) is not int:
             raise TypeError('width must be an integer')
-        if width < 0:
+        if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
 
@@ -31,9 +31,9 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Sets the height"""
-        if type(height) is not int:
+        if type(value) is not int:
             raise TypeError('height must be an integer')
-        if height < 0:
+        if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
 
