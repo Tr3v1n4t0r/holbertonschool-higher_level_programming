c7a6759a16e8f02a6029f7fd269047efa7636ca7
Tr3v1n4t0r <813@holbertonschool.com>
2020-01-23 16:12:49 -0800

    1 done

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is the "my list" module that supplies one function, print_sorted()
+"""
+
+class MyList(list):
+    """Represents a MyList class"""
+
+    def print_sorted(self):
+        """Prints the list in ascending order"""
+        print(sorted(self))
