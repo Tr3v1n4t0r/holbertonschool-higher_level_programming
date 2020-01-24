51a326e97ba48a5ad9d5946b67573d7bb9671d25
Tr3v1n4t0r <813@holbertonschool.com>
2020-01-24 14:30:27 -0800

    tasks 8 and 9 done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -38,6 +38,23 @@ class Rectangle(Base):
                 str(self.__y) + ' - ' + str(self.__width) + '/' +
                 str(self.__height))
 
+    def update(self, *args, **kwargs):
+        """Assigns an argument to each attribute"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.__width = args[1]
+            if len(args) >= 3:
+                self.__height = args[2]
+            if len(args) >= 4:
+                self.__x = args[3]
+            if len(args) >= 5:
+                self.__y = args[4]
+        elif kwargs:
+                for key, value in kwargs.items():
+                    setattr(self, key, value)
+
     @property
     def width(self):
         """Gets the width"""
