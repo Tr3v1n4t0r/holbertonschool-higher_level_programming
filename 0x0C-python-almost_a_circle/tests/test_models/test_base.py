8a15b71540c226d5734f4d7b682c88c3170d401f
Tr3v1n4t0r <813@holbertonschool.com>
2020-01-27 13:03:57 -0800

    added test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,11 +2,23 @@
 """
 Module for testing the Base class
 """
+import contextlib
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+from io import StringIO
 
 class TestClassBase(unittest.TestCase):
     """Test the Base class"""
+
+    def setUp(self):
+        """Sets up base class"""
+        Base._Base__nb_objects = 0
+
+    def tearDown(self):
+        """Finishing after test methods"""
+        pass
 
     def test_init(self):
         """Tests proper init"""
@@ -15,23 +27,41 @@ class TestClassBase(unittest.TestCase):
         self.assertEqual(type(a), Base)
         self.assertEqual(type(b), Base)
 
-    def test_id(self):
-        a = Base()
-        b = Base()
-        c = Base(2)
-        d = Base(50)
-        e = Base(-2)
-        f = Base(-50)
-        self.asserEqual(a.id, 1)
-        self.asserEqual(b.id, 2)
-        self.asserEqual(c.id, 3)
-        self.asserEqual(d.id, 50)
-        self.asserEqual(e.id, -2)
-        self.asserEqual(f.id, -50)
-
     def test_type(self):
+        """Tests type"""
         a = Base()
         self.assertTrue(type(a) == Base)
+
+    def test_id(self):
+        """Tests id"""
+        a = Base()
+        b = Base(12)
+        c = Base(-28)
+        self.assertEqual(a.id, 1)
+        self.assertEqual(b.id, 12)
+        self.assertEqual(c.id, -28)
+
+    def test_unknown(self):
+        """Tests name error"""
+        with self.assertRaises(NameError):
+            Base(a)
+
+    def test_empty_json(self):
+        """Tests to_json_string method with empty and None"""
+        json = Base.to_json_string('')
+        self.assertEqual(json, '[]')
+        json = Base.to_json_string(None)
+        self.assertEqual(json, '[]')
+
+    def test_json(self):
+        """Tests to_json_string method"""
+        a = Rectangle(12, 3, 9, 2)
+        dictionary = a.to_dictionary()
+        json = Base.to_json_string([dictionary])
+        self.assertEqual(len(json), len(str([{"x": 9, "width": 12, "id": 1,
+                                              "height": 3, "y": 2}])))
+        self.assertTrue(type(json), dict)
+        self.assertTrue(type(json) is str)
 
 if __name__ == '__main__':
     unittest.main()
