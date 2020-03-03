4304937ff2572d9413f67dac182810dafca7f4ef
Tr3v1n4t0r <813@holbertonschool.com>
2020-03-03 11:25:29 -0800

    typo

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- List all shows in hbtn_0d_tvshows with at least one genre linked
-SELECT tv_shows.title, tv_show_genre.genre_id FROM tv_shows LEFT JOIN tv_show_genre ON tv_shows.id = tv_show_genre.show_id WHERE tv_show_genre.genre_id IS NOT NULL OREDER BY tv_shows.title, tv_show_genre.genre_id;
+SELECT tv_shows.title, tv_show_genre.genre_id FROM tv_shows LEFT JOIN tv_show_genre ON tv_shows.id = tv_show_genre.show_id WHERE tv_show_genre.genre_id IS NOT NULL ORDER BY tv_shows.title, tv_show_genre.genre_id;
