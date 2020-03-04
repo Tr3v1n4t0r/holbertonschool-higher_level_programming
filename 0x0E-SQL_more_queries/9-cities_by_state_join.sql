c0dea07130712cb0045f2b87329b854ea485fcde
Tr3v1n4t0r <813@holbertonschool.com>
2020-03-03 23:22:37 -0800

    typo

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- List all cities in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name FROM states RIGHT JOIN cities ON cities.state_id _ states.id;
+SELECT cities.id, cities.name, states.name FROM states RIGHT JOIN cities ON cities.state_id = states.id;
