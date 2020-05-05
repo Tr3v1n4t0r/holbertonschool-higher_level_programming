91a88388f7e27793134aa96c06444008348882af
Tr3v1n4t0r <813@holbertonschool.com>
2020-05-05 12:37:04 -0700

    typo

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (process.argv[3]) {
-    console.log('Argument found');
+    console.log('Arguments found');
 } else if (process.argv[2]) {
     console.log('Argument found');
 } else {
